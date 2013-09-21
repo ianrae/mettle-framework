7a2726528a9c59687ede2c2b308e5dda4a421bbd
ianrae <windward37@hotmail.com>
2013-09-21 17:48:57 -0400

    more

--- src/org/mef/dalgen/unittests/ParseTests.java
+++ src/org/mef/dalgen/unittests/ParseTests.java
@@ -3,6 +3,7 @@ package org.mef.dalgen.unittests;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 import org.stringtemplate.v4.ST;
@@ -16,6 +17,16 @@ import sfx.SfxXmlReader;
 
 public class ParseTests extends BaseTest
 {
+	public static class FieldDef
+	{
+		public String name;
+	}
+	
+	public static class EntityDef
+	{
+		public String name;
+		public List<FieldDef> fieldL = new ArrayList<FieldDef>();
+	}
 
 	@Test
 	public void test() throws Exception
@@ -33,8 +44,38 @@ public class ParseTests extends BaseTest
 		Element el = p.getIthByName(entityEl, "field", 0);
 		assertEquals("id:Long", el.getTextContent());
 		
-		//		ArrayList<Element> list = p.getAllByXPath(doc, "//field");
-//		assertEquals(2, list.size());
+		EntityDef def = new EntityDef();
+		def.name = getEl(entityEl, "name");
+		
+		assertEquals("Task", def.name);
+		
+		list = p.getAllByXPath(doc, "//field");
+		assertEquals(2, list.size());
+		
+		for(int i = 0; i < 10; i++)
+		{
+			Element tmp = p.getIthByName(entityEl, "field", i);
+			if (tmp != null)
+			{
+				FieldDef fdef = new FieldDef();
+				fdef.name = getBody(tmp);
+				def.fieldL.add(fdef);
+			}
+		}
+		
+		assertEquals(2, def.fieldL.size());
+		assertEquals("id:Long", def.fieldL.get(0).name);
+	}
+	
+	private String getEl(Element el, String name)
+	{
+		String s = el.getAttribute(name);
+		return s.trim();
+	}
+	private String getBody(Element el)
+	{
+		String s = el.getTextContent();
+		return s.trim();
 	}
 
 }
