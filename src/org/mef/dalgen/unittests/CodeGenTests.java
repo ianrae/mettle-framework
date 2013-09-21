f124fab6a7a3b8acc694508c074234449b7373b2
ianrae <windward37@hotmail.com>
2013-09-21 19:38:36 -0400

    entity.stg

--- src/org/mef/dalgen/unittests/CodeGenTests.java
+++ src/org/mef/dalgen/unittests/CodeGenTests.java
@@ -3,12 +3,99 @@ package org.mef.dalgen.unittests;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.mef.dalgen.parser.DalGenXmlParser;
+import org.mef.dalgen.parser.EntityDef;
+import org.mef.dalgen.parser.FieldDef;
+import org.stringtemplate.v4.ST;
+import org.stringtemplate.v4.STGroup;
+import org.stringtemplate.v4.STGroupFile;
 
-public class CodeGenTests {
+import sfx.SfxBaseObj;
+import sfx.SfxContext;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+public class CodeGenTests extends BaseTest
+{
+	public static class EntityCodeGen extends SfxBaseObj
+	{
+		private String _path;
+		STGroup _group;
+
+		public EntityCodeGen(SfxContext ctx, String path)
+		{
+			super(ctx);
+			_path = path;
+			_group = new STGroupFile(_path);
+		}
+		
+		public String generate(EntityDef def)
+		{
+			ST st = _group.getInstanceOf("classdecl");
+			st.add("type", "int");
+			st.add("name", def.name);
+			String result = st.render(); 
+			
+			result += genFields(def);
+			
+			st = _group.getInstanceOf("endclassdecl");
+			result += st.render(); 
+			
+			return result;
+		}
+		
+		private String genFields(EntityDef def)
+		{
+			
+			String result = "";
+			for(FieldDef fdef : def.fieldL)
+			{
+				result += this.buildField(fdef);
+				result += "\n\n";
+			}
+			return result;
+		}
+		
+		private String buildField(FieldDef fdef)
+		{
+			ST st = _group.getInstanceOf("fielddecl");
+			String result = "";
+			st.add("type", fdef.typeName);
+			st.add("name", fdef.name);
+			result = st.render(); 
+
+			String s = "";
+			for(String ann : fdef.annotationL)
+			{
+				if (s.isEmpty())
+				{
+					s += "   ";
+				}
+				s += ann + " ";
+			}
+			if (! s.isEmpty())
+			{
+				s += "\n";
+			}
+			
+			result = s + result;
+			return result;
+		}
 	}
+	@Test
+	public void test() throws Exception
+	{
+		log("--test--");
+		SfxContext ctx = new SfxContext();
+		String path = this.getTestFile("dalgen.xml");
+		DalGenXmlParser parser = new DalGenXmlParser(ctx);
+		boolean b = parser.parse(path);
 
+		assertEquals(1, parser._entityL.size());
+		EntityDef def = parser._entityL.get(0);
+		
+		path = this.getTestFile("entity.stg");
+		EntityCodeGen gen = new EntityCodeGen(ctx, path);
+		String code = gen.generate(def);	
+		log(code);
+		assertEquals(true, 10 < code.length());
+	}
 }
