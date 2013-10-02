de128c84dabbb4bb60889a11c46d4bf7bad0d8fd
ianrae <windward37@hotmail.com>
2013-10-01 20:15:31 -0400

    more

--- code/framework/test/BeanDBTests.java
+++ code/framework/test/BeanDBTests.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -21,6 +22,90 @@ public class BeanDBTests
 		}
 	}
 	
+	public static class EntityDB
+	{
+		private List<Flight> union(List<Flight> L, List<Flight> L2) 
+		{
+			ArrayList<Flight> L3 = new ArrayList<BeanDBTests.Flight>();
+			for(Flight f : L)
+			{
+				L3.add(f);
+			}
+			for(Flight f : L2)
+			{
+				L3.add(f);
+			}
+			
+			return L3;
+		}
+		
+		private List<Flight> intersection(List<Flight> L, List<Flight> L2) 
+		{
+			ArrayList<Flight> L3 = new ArrayList<BeanDBTests.Flight>();
+			for(Flight f : L)
+			{
+				if (L2.contains(f))
+				{
+					L3.add(f);
+				}
+			}
+			
+			return L3;
+		}
+		
+		public boolean isMatch(Flight obj, String fieldName, String valueToMatch) 
+		{
+			if (fieldName == null)
+			{
+				return false;
+			}
+			
+			Field field = null;
+			Object value = null;
+			try 
+			{
+				field = obj.getClass().getDeclaredField(fieldName);
+				field.setAccessible(true);
+				value = field.get(obj);
+			}
+			catch (SecurityException e) 
+			{
+				// TODO Auto-generated catch block
+//				e.printStackTrace();
+			} 
+			catch (NoSuchFieldException e) 
+			{
+				// TODO Auto-generated catch block
+//				e.printStackTrace();
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+//				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+//				e.printStackTrace();
+			}
+			
+			if (value == null)
+			{
+				return false;
+			}
+			String s = value.toString();
+			return (s.equalsIgnoreCase(valueToMatch));
+		}
+
+		public Flight findFirstMatch(List<Flight> L, String fieldName, String valueToMatch) 
+		{
+			for(Flight f : L)
+			{
+				if (isMatch(f, fieldName, valueToMatch))
+				{
+					return f;
+				}
+			}
+			return null;
+		}
+
+	}
 	
 	@Test
 	public void testOr() 
@@ -31,7 +116,8 @@ public class BeanDBTests
 		List<Flight> L2 = this.buildFlights();
 		assertEquals(3, L2.size());
 		
-		List<Flight> L3 = union(L, L2);
+		EntityDB db = new EntityDB();
+		List<Flight> L3 = db.union(L, L2);
 		assertEquals(6, L3.size());
 		chkAllUnique(L, L2, L3);
 	}
@@ -44,6 +130,7 @@ public class BeanDBTests
 		assertEquals(3, L.size());
 		L.add(L.get(0)); //add dups
 		
+		EntityDB db = new EntityDB();
 		assertEquals(false, ensureUnique(L));
 	}
 	
@@ -56,7 +143,8 @@ public class BeanDBTests
 		List<Flight> L2 = this.buildFlights();
 		assertEquals(3, L2.size());
 		
-		List<Flight> L3 = this.intersection(L, L2);
+		EntityDB db = new EntityDB();
+		List<Flight> L3 = db.intersection(L, L2);
 		assertEquals(0, L3.size());
 		chkAllUnique(L, L2, L3);
 	}
@@ -71,13 +159,46 @@ public class BeanDBTests
 		assertEquals(3, L2.size());
 		L2.add(L.get(0));
 		
-		List<Flight> L3 = this.intersection(L, L2);
+		EntityDB db = new EntityDB();
+		List<Flight> L3 = db.intersection(L, L2);
 		assertEquals(1, L3.size());
 		chkAllUnique(L, L2, L3);
 		assertSame(L.get(0), L3.get(0));
 	}
 
+	@Test
+	public void testIsMatch() throws Exception
+	{
+		log("--testFind--");
+		List<Flight> L = this.buildFlights();
+		
+		Flight one = L.get(0);
+		EntityDB db = new EntityDB();
+		boolean b = db.isMatch(one, "flight", "abc");
+		assertEquals(false, b);
+		assertEquals(true, db.isMatch(one, "flight", "UL900"));
+		assertEquals(true, db.isMatch(one, "flight", "ul900"));
+		
+		assertEquals(false, db.isMatch(one, "flight", ""));
+		assertEquals(false, db.isMatch(one, "flight", null));
+		assertEquals(false, db.isMatch(one, "", null));
+		assertEquals(false, db.isMatch(one, null, null));
+	}
 	
+	@Test
+	public void testFindMatch() throws Exception
+	{
+		log("--testFindMatch--");
+		List<Flight> L = this.buildFlights();
+		
+		EntityDB db = new EntityDB();
+		Flight f = db.findFirstMatch(L, "flight", "abc");
+		assertEquals(null, f);
+		
+		assertSame(L.get(0), db.findFirstMatch(L, "flight", "UL900"));
+		assertSame(L.get(1), db.findFirstMatch(L, "flight", "AC710"));
+		assertSame(L.get(2), db.findFirstMatch(L, "flight", "AC900"));
+	}
 	
 	private void chkAllUnique(List<Flight> L, List<Flight> L2, List<Flight> L3)
 	{
@@ -90,34 +211,6 @@ public class BeanDBTests
 		assertEquals(true, ensureUnique(L));
 	}
 	
-	private List<Flight> union(List<Flight> L, List<Flight> L2) 
-	{
-		ArrayList<Flight> L3 = new ArrayList<BeanDBTests.Flight>();
-		for(Flight f : L)
-		{
-			L3.add(f);
-		}
-		for(Flight f : L2)
-		{
-			L3.add(f);
-		}
-		
-		return L3;
-	}
-	
-	private List<Flight> intersection(List<Flight> L, List<Flight> L2) 
-	{
-		ArrayList<Flight> L3 = new ArrayList<BeanDBTests.Flight>();
-		for(Flight f : L)
-		{
-			if (L2.contains(f))
-			{
-				L3.add(f);
-			}
-		}
-		
-		return L3;
-	}
 	
 	private boolean ensureUnique(List<Flight> L)
 	{
