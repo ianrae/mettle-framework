ddf40ce70608cb16a1f12f23e4a269bdcc9cca82
ianrae <windward37@hotmail.com>
2013-10-04 21:40:18 -0400

    dao

--- code/tools/dalgen/src/org/mef/dalgen/codegen/generators/RealDAOCodeGen.java
+++ code/tools/dalgen/src/org/mef/dalgen/codegen/generators/RealDAOCodeGen.java
@@ -1,5 +1,8 @@
 package org.mef.dalgen.codegen.generators;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.mef.dalgen.parser.EntityDef;
 import org.mef.dalgen.parser.FieldDef;
 import org.stringtemplate.v4.ST;
@@ -23,7 +26,9 @@ public class RealDAOCodeGen extends CodeGenBase
 		st.add("name", getClassName(def));
 		st.add("type", def.name);
 		result += st.render(); 
-		
+
+		result += genTouchAll(def);
+		result += genTouchAll2(def);
 		result += genQueries(def);
 		result += genMethods(def);
 		st = _group.getInstanceOf("endclassdecl");
@@ -32,6 +37,51 @@ public class RealDAOCodeGen extends CodeGenBase
 		return result;
 	}
 	
+	private String genTouchAll(EntityDef def) 
+	{
+		String result = "";
+		ST st = _group.getInstanceOf("touchall1");
+		
+		List<String> assignsL = new ArrayList<String>();
+		for(FieldDef fdef : def.fieldL)
+		{
+			if (fdef.name.equals("id"))
+			{
+				continue;
+			}
+			
+			String s = String.format("t.set%s(entity.%s);", uppify(fdef.name), fdef.name);
+			assignsL.add(s);
+		}
+		st.add("assigns", assignsL);
+		
+		result = st.render(); 
+		result += "\n\n";
+		return result;
+	}
+	private String genTouchAll2(EntityDef def) 
+	{
+		String result = "";
+		ST st = _group.getInstanceOf("touchall2");
+		
+		List<String> assignsL = new ArrayList<String>();
+		for(FieldDef fdef : def.fieldL)
+		{
+			if (fdef.name.equals("id"))
+			{
+				continue;
+			}
+			
+			String s = String.format("entity.%s = t.get%s();", fdef.name, uppify(fdef.name));
+			assignsL.add(s);
+		}
+		st.add("assigns", assignsL);
+		
+		result = st.render(); 
+		result += "\n\n";
+		return result;
+	}
+
 	@Override
 	public String getClassName(EntityDef def)
 	{
