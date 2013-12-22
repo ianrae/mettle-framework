dab4719c332daf07ca1fe60839f9866a097a7190
ianrae <windward37@hotmail.com>
2013-12-22 15:49:48 -0500

    more

---
+++ code/mettle/test/clog/ICLOGDAO.java
@@ -0,0 +1,16 @@
+package clog;
+
+import java.util.List;
+
+import org.mef.framework.dao.IDAO;
+
+import clog.TMTests.ClogEntity;
+
+public interface ICLOGDAO  extends IDAO
+{
+	ClogEntity findById(long id);
+	List<ClogEntity> all();
+	void save(ClogEntity entity);        
+	void update(ClogEntity entity);
+
+}
--- code/mettle/test/clog/TMTests.java
+++ code/mettle/test/clog/TMTests.java
@@ -1,35 +1,62 @@
 package clog;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
+import org.mef.framework.binder.IFormBinder;
+import org.mef.framework.entities.Entity;
 import org.mef.framework.sfx.SfxBaseObj;
 import org.mef.framework.sfx.SfxContext;
-import org.mef.framework.sfx.SfxErrorTracker;
 
 import persistence.WorldParser;
 import clog.JsonTests.AirportParser;
 import clog.JsonTests.BigAirport;
 import clog.JsonTests.BigAirportParser;
 import clog.JsonTests.GateParser;
-import clog.JsonTests.MultiAirport;
 import clog.JsonTests.MultiAirportParser;
 
-import tools.BaseTest;
-
-public class TMTests extends BaseJsonTest
+public class TMTests extends BaseJsonTest implements ICLOGDAO
 {
+	public class ClogEntity extends Entity
+	{
+		public ClogEntity()
+		{}
+
+		public ClogEntity(Long userId, String blob)
+		{
+			this.userId = userId;
+			this.blob = blob;
+		}
+
+		public ClogEntity(ClogEntity entity)
+		{
+			this.id = entity.id;
+			this.userId = entity.userId;
+			this.blob = entity.blob;
+		}
+
+		public Long id;
+		public Long userId;
+	    public String blob; //holds all the json
+	}
+	
+	
 	public static class AirportThingManager extends SfxBaseObj
 	{
 		private BigAirport loadedObj;
 		private boolean loaded; //have attempted a load
+		ICLOGDAO dao;
 		
 		public AirportThingManager(SfxContext ctx)
 		{
 			super(ctx);
+			dao = (ICLOGDAO) ctx.getServiceLocator().getInstance(ICLOGDAO.class);
 		}
 
-		public boolean load(String json) throws Exception 
+		private boolean load(String json) throws Exception 
 		{
 			WorldParser parser = createWorldParser();
 			loadedObj = (BigAirport) parser.parse(json);
@@ -43,11 +70,12 @@ public class TMTests extends BaseJsonTest
 			return loadedObj;
 		}
 		
-		public BigAirport load() throws Exception
+		public BigAirport load(Long id) throws Exception
 		{
 			if (! loaded)
 			{
-				load("");
+				ClogEntity entity = dao.findById(id);
+				load(entity.blob);
 			}
 			return loadedObj;
 		}
@@ -68,18 +96,19 @@ public class TMTests extends BaseJsonTest
 	public void test() throws Exception
 	{
 		init();
+		ICLOGDAO dao = this;
+		_ctx.getServiceLocator().registerSingleton(ICLOGDAO.class, dao);
+		
 		AirportThingManager tm = new AirportThingManager(_ctx);
 		
-		String json = getJson();
-		boolean b = tm.load(json);
-		assertEquals(true, b);
-		
-		BigAirport airport = tm.getLoadedObj();
+		Long clogId = 1L;
+		BigAirport airport = tm.load(clogId);
 		assertEquals(1, airport.id);
 	}
 	
 	
 	
+	//--- helpers ---
 	private String getJson() 
 	{
 		String s = "{'rootType':'BigAirport','root': {'id':1,'flag':true,'name':'bob','gate':{'id':2},'size':56}, 'refs': [ {'type': 'Gate', 'things': [ {'id':2,'name':'gate1'} ] } ] }";
@@ -87,8 +116,40 @@ public class TMTests extends BaseJsonTest
 	}
 
 
+	//------ implement ICLOGDAO ----
+	@Override
+	public int size() {
+		return 1;
+	}
+	@Override
+	public void delete(long id) {
+	}
+	@Override
+	public void updateFrom(IFormBinder binder) {
+	}
+	@Override
+	public ClogEntity findById(long id) 
+	{
+		ClogEntity entity = new ClogEntity();
+		entity.blob = this.getJson();
+		return entity;
+	}
 
-	//--- helpers ---
-	
+
+	@Override
+	public List<ClogEntity> all() 
+	{
+		ArrayList<ClogEntity> L = new ArrayList<TMTests.ClogEntity>();
+		L.add(findById(1));
+		return L;
+	}
+	@Override
+	public void save(ClogEntity entity) 
+	{
+	}
+	@Override
+	public void update(ClogEntity entity)
+	{
+	}
 
 }
