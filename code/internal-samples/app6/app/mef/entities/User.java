ddbcc2cf2d438aaded4d957602b10fdab9734ea2
ianrae <windward37@hotmail.com>
2014-01-26 16:48:56 -0500

    fix ctors

--- code/internal-samples/app6/app/mef/entities/User.java
+++ code/internal-samples/app6/app/mef/entities/User.java
@@ -2,29 +2,22 @@
 
 package mef.entities;
 
-import mef.gen.User_GEN;
-
 import org.mef.framework.entities.Entity;
+import mef.gen.*;
+import mef.entities.*;
 import java.util.Date;
-
 public class User extends User_GEN
 {
-	public User()
-	{}
+public User()
+{}
+        public User( String name)
+        {
+                this.name = name;
+        }
 
-
-	public User( String name)
-	{
-		this.name = name;
-	}
-
-	public User(User entity)
-	{
-		this.id = entity.id;
-		this.name = entity.name;
-	}
-
-
-
-
+        public User(User entity)
+        {
+                this.id = entity.id;
+                this.name = entity.name;
+        }
 }
--- code/internal-samples/app6/app/mef/gen/User_GEN.java
+++ code/internal-samples/app6/app/mef/gen/User_GEN.java
@@ -9,6 +9,20 @@ import java.util.Date;
 public class User_GEN extends Entity
 {
 
+        public User_GEN()
+        {
+        }
+
+        public User_GEN( String name)
+        {
+                this.name = name;
+        }
+
+        public User_GEN(User_GEN entity)
+        {
+                this.id = entity.id;
+                this.name = entity.name;
+        }
     public Long id;
 
     public String name;
