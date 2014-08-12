d6dd7c4262d8f22d06fedd0d94dfa411ab8600de
ianrae <windward37@hotmail.com>
2014-08-12 06:30:24 -0400

    minor fix to mgen

--- code/mettle/app/org/mef/framework/Version.java
+++ code/mettle/app/org/mef/framework/Version.java
@@ -1,8 +1,9 @@
 package org.mef.framework;
 
 public class Version {
-	public static final String version = "0.3.1.1";
+	public static final String version = "0.3.1.2";
 	
+	//v0.3.1.2 fix mgen
 	//v0.3.1.1 query processor observer
 	//v0.3.1.0 just to force heroku to get latest
 	//v0.3.0.1 add SSL stuff
