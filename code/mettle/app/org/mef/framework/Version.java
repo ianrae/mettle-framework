07030ae0532e2b9cc685ca188261200dcec7c764
ianrae <winfell808@gmail.com>
2014-10-04 19:52:53 -0400

    v0.4.1.2

--- code/mettle/app/org/mef/framework/Version.java
+++ code/mettle/app/org/mef/framework/Version.java
@@ -1,9 +1,9 @@
 package org.mef.framework;
 
 public class Version {
-	public static final String version = "0.4.1.1";
+	public static final String version = "0.4.1.2";
 	
-	//v0.4.1.1 query eqi case-insensitive
+	//v0.4.1.2 query eqi case-insensitive
 	//v0.4.1.0 SCAFFOLDGEN added
 	//v0.4.0.8 fix and IHasLongId
 	//v0.4.0.7 new binder using spring validation and IInput
